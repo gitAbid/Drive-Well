57f58b863919fcf99e61dffeb80b405668cdbe3f
Abid Hasan <abid.hasan157@northsouth.edu>
2018-02-23 11:39:37 +0600

    Auth is intialized

---
+++ app/src/main/java/com/drivewell/drivewell/ui/auth/login/ILoginPresenter.java
@@ -0,0 +1,8 @@
+package com.drivewell.drivewell.ui.auth.login;
+
+/**
+ * Created by abid on 2/23/18.
+ */
+
+public interface ILoginPresenter {
+}
---
+++ app/src/main/java/com/drivewell/drivewell/ui/auth/signup/ISignupPresenter.java
@@ -0,0 +1,8 @@
+package com.drivewell.drivewell.ui.auth.signup;
+
+/**
+ * Created by abid on 2/23/18.
+ */
+
+public interface ISignupPresenter {
+}
