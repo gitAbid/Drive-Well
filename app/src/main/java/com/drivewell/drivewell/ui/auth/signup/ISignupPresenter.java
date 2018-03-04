0f10dada377415e3f84783b05ccfa89f929ce1b8
Abid Hasan <abid.hasan157@northsouth.edu>
2018-03-04 21:56:44 +0600

    Form Validation Added

--- app/src/main/java/com/drivewell/drivewell/ui/auth/signup/ISignupPresenter.java
+++ app/src/main/java/com/drivewell/drivewell/ui/auth/signup/ISignupPresenter.java
@@ -1,13 +1,4 @@
 package com.drivewell.drivewell.ui.auth.signup;
-
-import android.app.Activity;
-import android.content.Context;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.TextInputEditText;
-import android.support.v7.widget.CardView;
-import android.view.View;
-import android.widget.Spinner;
-
 import com.drivewell.drivewell.model.User;
 
 /**
@@ -16,5 +7,4 @@ import com.drivewell.drivewell.model.User;
 
 public interface ISignupPresenter {
     void signUp(User user);
-    void init(Activity activity, CardView mLoginBack, FloatingActionButton mSignup, TextInputEditText mEmail, TextInputEditText mPassword, TextInputEditText mConfirmPassword, Spinner mUserType, TextInputEditText mName, TextInputEditText mAge, TextInputEditText mHomeAddress, TextInputEditText mContactNo);
 }
