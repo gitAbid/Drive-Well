234dfa22dc9437fc4c51f37be2cbec6af2710df5
Abid Hasan <abid.hasan157@northsouth.edu>
2018-03-04 21:28:23 +0600

    Signup Error Fixed

--- app/src/main/java/com/drivewell/drivewell/ui/auth/signup/SignupPresenter.java
+++ app/src/main/java/com/drivewell/drivewell/ui/auth/signup/SignupPresenter.java
@@ -1,15 +1,18 @@
 package com.drivewell.drivewell.ui.auth.signup;
 
 import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.widget.CardView;
-import android.view.View;
 import android.widget.Spinner;
 
-import com.drivewell.drivewell.database.firebase.FirebaseAuthentication;
 import com.drivewell.drivewell.model.User;
+import com.drivewell.drivewell.ui.LandingActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 /**
@@ -26,13 +29,30 @@ public class SignupPresenter implements ISignupPresenter {
     private Spinner mUserType;
 
     private Activity activity;
+    FirebaseAuth mAuth;
+
+    public SignupPresenter() {
+        mAuth=FirebaseAuth.getInstance();
+    }
 
     @Override
     public void signUp(User user) {
         boolean valid=validateUser(user);
         if (valid){
-            FirebaseAuthentication.getInstance().init(activity);
-            FirebaseAuthentication.getInstance().FirebaseSignUp(user);
+            mAuth.createUserWithEmailAndPassword(user.getEmail(),user.getPassword())
+                    .addOnSuccessListener(activity, new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+
+                            activity.startActivities(new Intent[] {new Intent(activity.getApplicationContext(), LandingActivity.class)});
+
+                        }
+                    }).addOnFailureListener(activity,new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                }
+            });
         }
     }
 
