eea7923e05bc5ec5fd2338f6c06df0d398b73151
Abid Hasan <abid.hasan157@northsouth.edu>
2018-03-26 14:54:34 +0600

    Nothing

--- app/src/main/java/com/drivewell/drivewell/utils/Utils.java
+++ app/src/main/java/com/drivewell/drivewell/utils/Utils.java
@@ -13,10 +13,6 @@ public class Utils {
     private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
     private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
 
-    public Utils() {
-    }
-
-
     public static String getTimeAgo(long time) {
         if (time < 1000000000000L) {
             // if timestamp given in seconds, convert to millis
